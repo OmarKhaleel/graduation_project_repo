5c5625bbd61ed128f05887d20bd1705feebda9b6
Omar Khalil <omar_khaleel2001@hotmail.com>
2024-03-11 18:48:28 +0300

    Added testing documentation inside text files for each testing folder.

--- test/bottom_navigation_bar_testing/bottom_navigation_bar_widget_testing.dart
+++ test/bottom_navigation_bar_testing/bottom_navigation_bar_widget_testing.dart
@@ -3,41 +3,32 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:palmear_application/presentation/widgets/bottom_navigation_bar.dart';
 
 void main() {
-  testWidgets('Bottom Navigation Bar Test', (WidgetTester tester) async {
-    // Build the bottom navigation bar widget
+  testWidgets('Bottom Navigation Bar Test - Check Selected Tab Color',
+      (WidgetTester tester) async {
     await tester.pumpWidget(
       MaterialApp(
         home: Scaffold(
           bottomNavigationBar: BottomNavigationBarScreen(
-            selectedIndex: 0,
+            selectedIndex: 0, // Assuming 'Home' is initially selected
             onItemSelected: (index) {},
           ),
         ),
       ),
     );
 
-    // Verify that the bottom navigation bar contains three items
-    expect(find.byType(BottomNavigationBar), findsOneWidget);
-    expect(find.byIcon(Icons.home), findsOneWidget);
-    expect(find.byIcon(Icons.map), findsOneWidget);
-    expect(find.byIcon(Icons.settings), findsOneWidget);
+    // Verify that the bottom navigation bar is present on the screen
+    final bottomNavigationBarFinder = find.byType(BottomNavigationBar);
+    expect(bottomNavigationBarFinder, findsOneWidget);
 
-    // Tap on the second navigation item (index 1)
-    await tester.tap(find.byIcon(Icons.map));
-    await tester.pump();
+    // Retrieve the BottomNavigationBar widget from the widget tester
+    final bottomNavigationBar =
+        tester.widget<BottomNavigationBar>(bottomNavigationBarFinder);
 
-    // Verify that the second navigation item is selected
-    expect(find.text('Home'), findsNothing);
-    expect(find.text('Map'), findsOneWidget);
-    expect(find.text('Settings'), findsNothing);
+    // Get the selected tab's background color
+    final selectedItemColor = bottomNavigationBar.selectedItemColor;
 
-    // Tap on the third navigation item (index 2)
-    await tester.tap(find.byIcon(Icons.settings));
-    await tester.pump();
-
-    // Verify that the third navigation item is selected
-    expect(find.text('Home'), findsNothing);
-    expect(find.text('Map'), findsNothing);
-    expect(find.text('Settings'), findsOneWidget);
+    // Check if the selected tab's color matches the expected color
+    expect(selectedItemColor,
+        const Color(0xFF6750A4)); // Use the default color for selected tabs
   });
 }
--- test/login_testing/unit_testing/login_unit_testing.dart
+++ test/login_testing/unit_testing/login_unit_testing.dart
@@ -1,61 +1,24 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:palmear_application/presentation/screens/home_screen.dart';
-import 'package:palmear_application/presentation/screens/login_screen.dart';
-
-class MockLoginScreen extends Mock implements LoginScreen {
-  @override
-  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
-    return '';
-  }
-}
+import 'package:palmear_application/data/services/auth_services.dart';
 
 void main() {
-  late MockLoginScreen loginScreen;
+  // Instance of AuthService
+  final authService = AuthService();
 
-  setUp(() {
-    loginScreen = MockLoginScreen();
-  });
+  group('AuthService', () {
+    test('authenticates successfully with valid credentials', () async {
+      expect(await authService.authenticate('example123@gmail.com', '123'),
+          isTrue);
+    });
 
-  testWidgets('Valid Credential Test Case', (WidgetTester tester) async {
-    await tester.pumpWidget(loginScreen);
+    test('authentication fails with invalid credentials', () async {
+      expect(
+          await authService.authenticate('user@example.com', 'wrongpassword'),
+          isFalse);
+    });
 
-    // valid credential
-    await tester.enterText(
-        find.byKey(const Key('email_field')), 'example123@gmail.com');
-    await tester.enterText(find.byKey(const Key('password_field')), '123');
-
-    await tester.tap(find.text('Login'));
-    await tester.pump();
-
-    expect(find.byType(MyHomePage), findsOneWidget);
-  });
-
-  testWidgets('Invalid Credential Test Case', (WidgetTester tester) async {
-    await tester.pumpWidget(loginScreen);
-
-    // invalid credentials
-    await tester.enterText(
-        find.byKey(const Key('email_field')), 'invalid@example.com');
-    await tester.enterText(
-        find.byKey(const Key('password_field')), 'invalidpassword');
-
-    await tester.tap(find.text('Login'));
-    await tester.pump();
-
-    // Verify that error message is shown
-    expect(find.text('Invalid email or password'), findsOneWidget);
-  });
-
-  testWidgets('Empty Email/Password Fields Test Case',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(loginScreen);
-
-    await tester.tap(find.text('Login'));
-    await tester.pump();
-
-    expect(find.byType(MyHomePage), findsNothing);
-    expect(find.text('Invalid email or password'), findsNothing);
+    test('authentication fails with empty email and password', () async {
+      expect(await authService.authenticate('', ''), isFalse);
+    });
   });
 }
