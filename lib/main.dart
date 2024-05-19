0efb5e813133f805324af5679b943ba462646783
Omar Khalil <omar_khaleel2001@hotmail.com>
2024-05-19 07:12:15 +0300

    Added splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,21 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:palmear_application/data/services/audio_services/audio_services.dart';
-import 'package:palmear_application/domain/use_cases/main_use_cases/load_current_user.dart';
-import 'package:palmear_application/presentation/widgets/main_widgets/main_widgets_provider.dart';
-import 'package:palmear_application/domain/entities/user_model.dart';
+import 'package:palmear_application/presentation/screens/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  AudioProcessor audioProcessor = AudioProcessor();
-  bool hasPermission = await audioProcessor.requestMicrophonePermission();
-  debugPrint('Microphone permission granted: $hasPermission');
-  UserModel? currentUser =
-      await loadCurrentUser(FirebaseAuth.instance.currentUser?.uid);
-  runApp(MyApp(currentUser: currentUser));
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  final UserModel? currentUser;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  const MyApp({super.key, this.currentUser});
-
-  @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MainWidgetsProvider(currentUser: widget.currentUser);
+    return const MaterialApp(
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
---
+++ lib/presentation/screens/splash_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:palmear_application/data/services/audio_services/audio_services.dart';
+import 'package:palmear_application/domain/use_cases/main_use_cases/load_current_user.dart';
+import 'package:palmear_application/presentation/widgets/main_widgets/main_widgets_provider.dart';
+import 'package:palmear_application/domain/entities/user_model.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToMain();
+  }
+
+  void _navigateToMain() async {
+    AudioProcessor audioProcessor = AudioProcessor();
+    bool hasPermission = await audioProcessor.requestMicrophonePermission();
+    debugPrint('Microphone permission granted: $hasPermission');
+    UserModel? currentUser =
+        await loadCurrentUser(FirebaseAuth.instance.currentUser?.uid);
+
+    await Future.delayed(
+      const Duration(seconds: 2),
+    ); // Adjust the delay as needed
+
+    Navigator.pushReplacement(
+      // ignore: use_build_context_synchronously
+      context,
+      MaterialPageRoute(
+        builder: (context) => MainWidgetsProvider(currentUser: currentUser),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('assets/images/palmear_logo.png'),
+      ),
+    );
+  }
+}
